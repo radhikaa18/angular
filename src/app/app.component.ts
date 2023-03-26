1ed838c68f1460ed3b02dcd40a2eef5e0d9727ce
Radhika Dhangare <radhikadhangare183@gmail.com>
2023-03-26 10:50:47 +0530

    login page code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'student';
+
+  user="";
+  pass="";
+  result="";
+  
+
+  login(){
+    if(this.user=="admin"&& this.pass=="admin123")
+    {
+      this.result="Login successful";
+      console.log("Login successful");
+    }
+    else{
+      console.log("Login failed");
+    }
+  }
+
 }
